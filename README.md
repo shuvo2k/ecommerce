87136d6fcdaf37961aefdc56ab941bb2aa9d3394
shuvo2k <findariful@gmail.com>
2018-08-16 07:51:52 +0600

    production init

---
+++ README.md
@@ -0,0 +1,5 @@
+# E-Commerce Using Django-2.0 and Python-3.6
+
+
+
+
---
+++ sample_wsgi.py
@@ -0,0 +1,17 @@
+import os 
+import sys  
+
+path = '/home/ariful/ecommerce'
+
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE']='ecommerce.settings'
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+application = StaticFilesHandler(get_wsgi_application())
+
+
+
